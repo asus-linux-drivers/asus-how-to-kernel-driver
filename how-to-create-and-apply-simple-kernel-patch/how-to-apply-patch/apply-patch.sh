969c12ccc4a21de40e4fe7d6ecc2408b322f4a81
Lukáš Drahník <ldrahnik@gmail.com>
2023-12-13 17:27:24 +0100

    Init

--- how-to-create-and-apply-simple-kernel-patch/how-to-apply-patch/apply-patch.sh
+++ how-to-create-and-apply-simple-kernel-patch/how-to-apply-patch/apply-patch.sh
@@ -12,11 +12,19 @@ fi
 
 echo "Using: $PATCH_FILE"
 
+rm -rf drivers
+
 # drivers/platform/x86/asus-wmi.c
+# drivers/platform/x86/asus-wmi.h
 mkdir -p drivers/platform/x86
 wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.c?h=linux-$VERSION.y" -O "drivers/platform/x86/asus-wmi.c"
-# include/linux/platform_data/x86/asus-wmi.h
-mkdir -p include/linux/platform_data/x86
-wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/include/linux/platform_data/x86/asus-wmi.h?h=linux-$VERSION.y" -O "include/linux/platform_data/x86/asus-wmi.h"
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.h?h=linux-$VERSION.y" -O "drivers/platform/x86/asus-wmi.h"
+
+# headers inside include/ are private ones so when building part of kernel like DKMS does and using them file has to be added manually in .h and be included .c file
+# (patch contains include with new path below)
+mkdir -p drivers/platform/x86/include
 
 patch -p1 < "${PATCH_FILE}.patch"
+
+sudo mkdir -p /usr/src/asus-wmi-1.0
+sudo cp -r . /usr/src/asus-wmi-1.0
