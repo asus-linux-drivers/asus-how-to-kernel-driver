a9e2aed3ec208713050398273bb2a527f85abc34
Lukáš Drahník <ldrahnik@gmail.com>
2023-12-13 13:59:40 +0100

    Init

--- how-to-create-and-apply-simple-kernel-patch/how-to-apply-patch/apply-patch.sh
+++ how-to-create-and-apply-simple-kernel-patch/how-to-apply-patch/apply-patch.sh
@@ -13,9 +13,10 @@ fi
 echo "Using: $PATCH_FILE"
 
 # drivers/platform/x86/asus-wmi.c
-wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.c?h=linux-$VERSION.y" -O 'drivers/platform/x86/asus-wmi.c'
+mkdir -p drivers/platform/x86
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.c?h=linux-$VERSION.y" -O "drivers/platform/x86/asus-wmi.c"
 # include/linux/platform_data/x86/asus-wmi.h
-wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/include/linux/platform_data/x86/asus-wmi.h?h=linux-$VERSION.y" -O 'include/linux/platform_data/x86/asus-wmi.h'
+mkdir -p include/linux/platform_data/x86
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/include/linux/platform_data/x86/asus-wmi.h?h=linux-$VERSION.y" -O "include/linux/platform_data/x86/asus-wmi.h"
 
-patch -p1 < $PATCHFILE
-rm *.orig
+patch -p1 < "${PATCH_FILE}.patch"
